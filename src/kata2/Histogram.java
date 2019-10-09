c27a39c4493009f05de5ef4e4d7c5a071ca87acd
Usuario <Usuario@10.22.148.98>
2019-10-09 09:24:04 +0100

    Kata2

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,25 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Histogram implements Iterable<Integer> {
+    private final Map<Integer, Integer> counts = new HashMap<>();
+    
+    public Histogram(int... values) {
+        for (int value : values) {
+            counts.put(value, counts.getOrDefault(value, 0)+1);
+        }
+    }
+
+    public int getCount(int value){
+        return counts.get(value);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return counts.keySet().iterator();
+    }
+    
+}
